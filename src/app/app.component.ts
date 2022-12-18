303f0cf3e0ea30afc26f200cfcfc346427585b6b
SelakaKithmal <selaka.kithmal@gmail.com>
2022-12-18 18:44:15 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  myFiles: any[] = [];
+  sMsg: string = '';
+  desc: any;
+  fileName: any;
+  ngOnInit() {}
 
-  myFiles:string [] = [];
-  sMsg:string = '';
-
-  ngOnInit () {  }
-
-  getFileDetails (e) {
-    //console.log (e.target.files);
-    for (var i = 0; i < e.target.files.length; i++) { 
-      this.myFiles.push(e.target.files[i]);
+  getFileDetails(e) {
+    console.log(e.target.files);
+    for (var i = 0; i < e.target.files.length; i++) {
+      if (e) {
+        this.setName(e.target.files[0].name);
+        const reader = new FileReader();
+        console.log(e.target.files[0].name);
+        reader.onload = this.handleReaderLoaded.bind(this);
+        reader.readAsBinaryString(e.target.files[0]);
+      }
     }
-    console.log(this.myFiles)
+    // console.log(this.myFiles);
   }
 
-  uploadFiles () {
+  setName(e) {
+    this.fileName =e;
+  }
+  handleReaderLoaded(e) {
+    console.log(e);
+    let val = {
+      name: this.fileName,
+      img: 'data:image/png;base64,' + btoa(e.target.result),
+    };
+    console.log(val);
+    this.myFiles.push(val);
+    this.fileName=null;
+  }
+
+  uploadFiles() {
     const frmData = new FormData();
-    
-    for (var i = 0; i < this.myFiles.length; i++) { 
-      frmData.append("fileUpload", this.myFiles[i]);
+    for (var i = 0; i < this.myFiles.length; i++) {
+      frmData.append('fileUpload', this.myFiles[i]);
     }
   }
 }
